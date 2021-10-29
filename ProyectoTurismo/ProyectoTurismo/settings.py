76fcb67d31ba301846e58f572d8272f8ad8af8d1
FaiverBravo <faivertkd@gmail.com>
2021-10-28 23:08:30 -0500

    admin interface y created

--- ProyectoTurismo/ProyectoTurismo/settings.py
+++ ProyectoTurismo/ProyectoTurismo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_interface',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,7 +45,10 @@ INSTALLED_APPS = [
     'servicios',
     'contacto',
     'tienda',
+    'colorfield',
 ]
+
+X_FRAME_OPTIONS='SAMEORIGIN'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
