52290c0cf69641a331962ff9e4deb939b67719bb
FAIVER <faivertkd@gmail.com>
2025-02-25 14:22:52 -0500

    Cambio de nombre de ProyectoTurismo a WakaTur

--- WakaTur/asgi.py
+++ WakaTur/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ProyectoTurismo project.
+ASGI config for WakaTur project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProyectoTurismo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WakaTur.settings')
 
 application = get_asgi_application()
--- WakaTur/wsgi.py
+++ WakaTur/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ProyectoTurismo project.
+WSGI config for WakaTur project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProyectoTurismo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WakaTur.settings')
 
 application = get_wsgi_application()
