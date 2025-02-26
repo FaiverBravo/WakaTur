84d70c0ab20d66eab8613e7816c67006ad237af7
FAIVER <faivertkd@gmail.com>
2025-02-26 09:55:01 -0500

    eliminacion de archivos duplicados

--- tienda/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import CategoriaProd, Producto
-
-# Register your models here.
-
-  
-admin.site.register(CategoriaProd)
-admin.site.register(Producto)
-
