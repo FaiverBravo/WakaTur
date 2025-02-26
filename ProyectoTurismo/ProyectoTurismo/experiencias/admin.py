be6ef192b9d1085ac3542d48e370c880cfc122ad
FAIVER <faivertkd@gmail.com>
2025-02-25 19:45:57 -0500

    quitando stegin de eliminados

--- ProyectoTurismo/ProyectoTurismo/experiencias/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Experiencia
-
-# Register your models here.
-
-class ExperienciaAdmin(admin.ModelAdmin):
-    readonly_fields=('created', 'updated')
-
-admin.site.register(Experiencia, ExperienciaAdmin)
--- ProyectoTurismo/ProyectoTurismo/gastronomia/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Gastronomia
-
-# Register your models here.
-
-class GastronomiaAdmin(admin.ModelAdmin):
-    readonly_fields=('created', 'updated')
-
-admin.site.register(Gastronomia, GastronomiaAdmin)
